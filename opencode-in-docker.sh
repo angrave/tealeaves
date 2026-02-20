3cb47c4520038926abc9e1bb17424cf2f2543411
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2026-02-19 18:16:22 -0600

    Fix typos in script comments

--- opencode-in-docker.sh
+++ opencode-in-docker.sh
@@ -6,8 +6,8 @@
 # This uses Docker (https://docs.docker.com/desktop/) to add some security; it isolates models from the rest of your system
 # You need to know how to use the terminal
 
-# Create a new directory e.g., "hello-ai", and change to that directory and run this script
-# It will mount that directory and download the offocial opencode docker image
+# Create a new directory e.g., "hello-ai", change to that directory and then run this script
+# It will mount that directory and download the official opencode docker image
 # Type /connect then search for litellm.kooper.org to connect to it (it will ask you for the api key)
 # Type "Hello" or "Create a file hello.py that prints hello world"
 # Type /exit to exit

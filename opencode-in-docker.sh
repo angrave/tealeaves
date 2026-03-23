e3dfe919dd02de319c6d70f0673462595412ef49
Lawrence Angrave <4468456+angrave@users.noreply.github.com>
2026-03-22 21:18:26 -0500

    Update opencode-in-docker.sh for improved functionality

--- opencode-in-docker.sh
+++ opencode-in-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # You can find the latest version of this script at https://raw.githubusercontent.com/angrave/tealeaves/refs/heads/gh-pages/opencode-in-docker.sh
 # ProTip: Read the offical opencode documentation!
-# 
+#
 # Quick Getting Started tips:
 # This uses Docker (https://docs.docker.com/desktop/) to add some security; it isolates models from the rest of your system
 # You need to know how to use the terminal
@@ -15,12 +15,29 @@
 export OPENCODE_CONFIG_CONTENT=$(cat <<EOF
 {
   "provider": {
-    "llm.ncsa.illinois.edu": {      
+    "llm.ncsa.illinois.edu": {
       "models": {
               "qwen3-coder-next": {"name": "qwen3-coder-next" }
               },
       "options": {
         "baseURL": "https://llm.ncsa.illinois.edu/"
+      }
+    },
+    "lumen.ncsa.illinois.edu": {
+      "models": {
+              "qwen3-coder-next": {"name": "qwen3-coder-next" },
+     "nemotron-cascade-2": {
+       "name": "nemotron-cascade-2",
+        "options": {
+          "reasoning_budget": 40096,
+          "temperature": 1.0,
+          "top_p": 0.95,
+          "systemPrompt": "You are an expert developer. For complex tasks, use <think> tags for reasoning. When you need to use a tool, wrap the call in <tool_call> tags. Available tools are listed below in <tools> tags. If necessary adjust tool calling and retry"
+      }
+    }
+              },
+      "options": {
+        "baseURL": "https://lumen.ncsa.illinois.edu/v1/"
       }
     }
   }
@@ -28,18 +45,42 @@ export OPENCODE_CONFIG_CONTENT=$(cat <<EOF
 EOF
 )
 
-# Use the latest official opencode image
-docker pull ghcr.io/anomalyco/opencode:latest
+GRAY='\033[0;37m'
+RED='\033[0;91m'
+YELLOW='\033[0;33m'
+NORM='\033[0m'
+
+
+if ! docker info >/dev/null ; then
+   printf "${RED}You need docker to be installed and running${NORM}"
+   return
+fi
+
+
+#  Change to true to skip searching for files that belog to root
+SKIP_ROOT_CHECK=false
+
+if [ "$(id -u)" -ne 0 ] && [ "$SKIP_ROOT_CHECK" != "true" ] ; then
+   printf "Checking for root-owned files in ${GRAY}~/.local/share${NORM} and ${GRAY}.${RED}\n"
+   if find ~/.local/share .  -user root -print -quit | grep . ; then
+     printf "${NORM}There is at least one file or directory owned as root.\nPlease fix e.g,\n"
+     printf "${YELLOW}sudo chown -R $USER:$USER ~/.local/share .${NORM}\n\n"
+     printf "Or set ${YELLOW}SKIP_ROOT_CHECK=true${NORM} in ${YELLOW}$0${NORM}\n"
+     return 1
+   fi
+   printf "${NORM}"
+fi
+
+
+mkdir -p ~/.local/share/opencode-dockerized
+
+OPENCODE_IMAGE="ghcr.io/anomalyco/opencode:latest"
+
+# Comment out this line if you don't automatically want the latest version
+docker pull "$OPENCODE_IMAGE"
 
 # The -v mount syntax creates our own ~/.local/share/opencode-dockerized if it does not exist
 # You could mount ~/.local/share/opencode instead
-# 
-if find ~/.local/share  -user root -print -quit | grep -q .
-then
-echo -e "Earlier versions of created configuration files and directories as root. Please fix then run me again e.g, \nsudo chown -R $USER:$USER ~/.local/share ."
-else
-
-mkdir -p ~/.local/share/opencode-dockerized
 docker run -it --rm \
   -u $(id -u):$(id -g) \
   -e OPENCODE_CONFIG_CONTENT \
@@ -47,7 +88,4 @@ docker run -it --rm \
   --workdir /workspace \
   -v "$(pwd):/workspace" \
   -v ~/.local/share/opencode-dockerized/:/tmp/opencode/ \
-  ghcr.io/anomalyco/opencode:latest "$@"
-fi
-
-
+  "$OPENCODE_IMAGE" "$@"
